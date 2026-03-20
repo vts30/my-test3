05fc66091d655e7a411c412ca82c463aa0dd1255
vts30 <pulak30@gmail.com>
2026-03-20 16:30:52 +0100

    Revert to $env for credentials in node1

--- workflows/node1-source-config.js
+++ workflows/node1-source-config.js
@@ -8,8 +8,8 @@ return [
       id: 'tagesspiegel',
       listUrl:       'https://background.tagesspiegel.de/digitalisierung-und-ki',
       loginUrl:      'https://background.tagesspiegel.de/login',
-      email:         process.env.TS_EMAIL,
-      password:      process.env.TS_PASSWORD,
+      email:         $env.TS_EMAIL,
+      password:      $env.TS_PASSWORD,
       requiresLogin: true,
       source:        'tagesspiegel',
     }
